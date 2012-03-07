2cf8a3dfb626155a1b1d025de893a15bede87176
Daniel Spaude <daniel@spaude.de>
2012-03-07 13:01:37 +0100

    register ApplicationHelper inside of engine.rb
    so that modified application helpers are don't need a server restart

--- lib/goldencobra_newsletter/engine.rb
+++ lib/goldencobra_newsletter/engine.rb
@@ -2,5 +2,14 @@
 module GoldencobraNewsletter
   class Engine < ::Rails::Engine
     isolate_namespace GoldencobraNewsletter
+
+    config.to_prepare do
+      ApplicationController.helper(GoldencobraNewsletter::ApplicationHelper)
+      ActionController::Base.helper(GoldencobraNewsletter::ApplicationHelper)
+
+      # ApplicationController.helper(GoldencobraNewsletter::NewsletterHelper)
+      # ActionController::Base.helper(GoldencobraNewsletter::NewsletterHelper)
+    end
+
   end
 end
