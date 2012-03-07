f063d5c6d04619513c039a03fba03858515096b1
Daniel Spaude <daniel@spaude.de>
2012-03-07 16:32:02 +0100

    adding controller for newsletter register

---
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -0,0 +1,11 @@
+module GoldencobraNewsletter
+  class NewslettersController < ApplicationController
+
+    def register
+
+      puts "REGISTER WURDE AUFGERUFEN!!!!!!!!!!!!!!!!"
+
+    end
+
+  end
+end
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
   devise_for :users, ActiveAdmin::Devise.config
+  mount GoldencobraNewsletter::Engine => "/goldencobra_newsletter"
   mount Goldencobra::Engine => "/"
-  mount GoldencobraNewsletter::Engine => "/newsletter"
 end
