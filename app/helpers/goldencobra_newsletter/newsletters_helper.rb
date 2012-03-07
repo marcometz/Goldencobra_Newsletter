2e1d565ef34eeef5c30751d12991589444c4b282
Daniel Spaude <daniel@spaude.de>
2012-03-07 13:18:21 +0100

    register_newsletter_form partial prototyping

--- app/helpers/goldencobra_newsletter/newsletters_helper.rb
+++ app/helpers/goldencobra_newsletter/newsletters_helper.rb
@@ -1,7 +1,13 @@
 module GoldencobraNewsletter
   module NewslettersHelper
     def newsletter_registration_form
-      render :text => "HIER KOMMT DAS TOLLE FORM REIN"
+      # render :text => "HIER KOMMT DAS TOLLE FORM REIN"
+
+      # return_content += content_tag(:div, render(:partial => "goldencobra_events/events/user"), :style => "display:none", :id => "goldencobra_events_enter_account_data_form")
+
+      render :partial => "goldencobra_newsletter/newsletters/register"
+
+
     end
 
   end
