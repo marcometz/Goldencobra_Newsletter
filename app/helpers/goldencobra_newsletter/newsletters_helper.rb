d22703bf70adf32fbdfbb31e1d38ca83a2f5b1c0
Daniel Spaude <daniel@spaude.de>
2012-03-07 15:35:49 +0100

    work on helpers for newsletter registration form

--- app/helpers/goldencobra_newsletter/newsletters_helper.rb
+++ app/helpers/goldencobra_newsletter/newsletters_helper.rb
@@ -6,7 +6,23 @@ module GoldencobraNewsletter
       # return_content += content_tag(:div, render(:partial => "goldencobra_events/events/user"), :style => "display:none", :id => "goldencobra_events_enter_account_data_form")
 
       render :partial => "goldencobra_newsletter/newsletters/register"
+    end
 
+    def helper_neu(field_name, options = {})
+
+      css_prefix = "goldencobra-newsletter-registration"
+
+      content_tag :div, :class => css_prefix do
+
+        if options[:required]
+          "DER NEUE HELPER: #{field_name} -- #{options}"
+        end
+      end
+
+  # <div class="#{css_prefix}-field-group">
+  #   <%= label_tag(:title, "Titel") %>
+  #   <%= text_field_tag(:title, nil, :id => "#{css_prefix}-field-title") %>
+  # </div>
 
     end
 
