2f9cfe7d7e553ae7589d74ff82ec8fa24e52a7ec
Daniel Spaude <daniel@spaude.de>
2012-03-08 10:41:37 +0100

    work on newsletter

--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -12,13 +12,12 @@ module GoldencobraNewsletter
 
       else
 
-        user = User.create(:firstname => "daniel", :lastname => "Spaude", :email => "daniel@spaude.de", :password => "pass12345", :password_confirmation => "pass12345")
+        generated_pass = Digest::MD5.new.hexdigest("pass-#{Time.now.to_f}")
+
+        user = User.create(:firstname => params[:firstname], :lastname => [:lastname], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
 
 
       end
-
-      # render :text => "user.lastname " + user.lastname + "; old_newsletter: " + old_newsletter.to_s
-
     end
 
   end
