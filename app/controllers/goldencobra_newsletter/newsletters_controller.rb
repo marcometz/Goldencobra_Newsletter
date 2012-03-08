ca4bc0e5d1c3a988125b5bb159f5c51960e2a3cd
Daniel Spaude <daniel@spaude.de>
2012-03-08 12:25:53 +0100

    some stuff works now in controller

--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -8,22 +8,23 @@ module GoldencobraNewsletter
 
       user = User.find_by_email(params[:email])
 
-      if user == nil
+      if(user == nil)
 
         generated_pass = Digest::MD5.new.hexdigest("pass-#{Time.now.to_f}")
-        user = User.create(:firstname => params[:firstname], :lastname => [:lastname], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
+        user = User.create(:firstname => params[:first_name], :lastname => params[:last_name], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
 
         user.save!
 
       end
 
-      if user.newsletter_registration == nil
+      if(user.newsletter_registration == nil)
         user.newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.new
       end
 
-      user.company_name = params[:company_name]
-      user.is_subscriber = true
+      user.newsletter_registration.company_name = params[:company]
+      user.newsletter_registration.is_subscriber = true
 
+      user.newsletter_registration.save!
       user.save!
 
 
