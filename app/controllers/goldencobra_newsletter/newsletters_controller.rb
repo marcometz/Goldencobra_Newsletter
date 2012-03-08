327f91842f968925b0bcaecfe03ce0975642b5f0
Daniel Spaude <daniel@spaude.de>
2012-03-08 12:49:32 +0100

    error handling in js for newsletter

--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -6,24 +6,23 @@ module GoldencobraNewsletter
       # wenn keien fehler: formular aus dom entfernen und danke hinschreiben
       # in session als newsletter-abonentn markieren, so dass formular nicht nochmal angezegit wird
 
-      user = User.find_by_email(params[:email])
+      @user = User.find_by_email(params[:email])
 
-      if(user == nil)
+      if(@user == nil)
 
         generated_pass = Digest::MD5.new.hexdigest("pass-#{Time.now.to_f}")
-        user = User.create(:firstname => params[:first_name], :lastname => params[:last_name], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
+        @user = User.create(:firstname => params[:first_name], :lastname => params[:last_name], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
 
       end
 
-      if(user.newsletter_registration == nil)
-        user.newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.new
+      if(@user.newsletter_registration == nil)
+        @user.newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.new
       end
 
-      user.newsletter_registration.company_name = params[:company]
-      user.newsletter_registration.is_subscriber = true
+      @user.newsletter_registration.company_name = params[:company]
+      @user.newsletter_registration.is_subscriber = true
 
-      user.save!
-
+      @success = @user.save
 
     end
 
