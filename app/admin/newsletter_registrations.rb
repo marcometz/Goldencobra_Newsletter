7895da877347e09a9ceefa42dac7db4548b5650f
Daniel Spaude <daniel@spaude.de>
2012-03-08 14:43:53 +0100

    work on active admin for newsletter

--- app/admin/newsletter_registrations.rb
+++ app/admin/newsletter_registrations.rb
@@ -12,6 +12,52 @@ ActiveAdmin.register GoldencobraNewsletter::NewsletterRegistration, :as => "News
   end
 
 
+  show :title => newsletter_registration.user.lastname do
+    panel "User" do
+      attributes_table_for newsletter_registration.user do
+        # [:firstname, :lastname, :title, :email, :gender, :function, :phone, :fax, :facebook, :twitter, :linkedin, :xing, :googleplus, :created_at, :updated_at].each do |aa|
+        #   row aa
+      end
+    end
+  end
+    # panel "Company" do
+    #   if applicant.company
+    #     attributes_table_for applicant.company do
+    #       [:title, :legal_form, :phone, :fax, :homepage, :sector].each do |aa|
+    #         row aa
+    #       end
+    #     end
+    #     if applicant.company.location
+    #       attributes_table_for applicant.company.location do
+    #         [:street, :zip, :city, :region, :country].each do |aa|
+    #           row aa
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
+    # panel "Registrations" do
+    #   table do
+    #     tr do
+    #       ["Event title", "Pricegroup title", "Price", "Registered at"].each do |sa|
+    #         th sa
+    #       end
+    #     end
+    #     applicant.event_registrations.each do |ereg|
+    #       tr do
+    #         [ereg.event_pricegroup.event.title, ereg.event_pricegroup.title, number_to_currency(ereg.event_pricegroup.price, :locale => :de), ereg.created_at].each do |esa|
+    #           td esa
+    #         end
+    #       end
+    #     end
+    #     tr :class => "total" do
+    #       td ""
+    #       td ""
+    #       td number_to_currency(applicant.total_price, :locale => :de)
+    #       td ""
+    #     end
+    #   end
+
 
   # form :html => { :enctype => "multipart/form-data" } do |f|
   #   f.inputs "Allgemein" do
