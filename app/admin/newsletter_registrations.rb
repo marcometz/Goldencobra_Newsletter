ac0a0224e78a5089a673362a3804e10be352ff6a
Marco Metz <marco.metz@gmail.com>
2012-03-08 16:54:36 +0100

    modified:   .rvmrc 	modified:   app/admin/newsletter_registrations.rb 	modified:   app/controllers/goldencobra_newsletter/newsletters_controller.rb

--- app/admin/newsletter_registrations.rb
+++ app/admin/newsletter_registrations.rb
@@ -22,68 +22,11 @@ ActiveAdmin.register GoldencobraNewsletter::NewsletterRegistration, :as => "News
             row aa
           end
         end
-
       end
       row :company_name
       row :is_subscriber
-      # row :image do
-      #   image_tag(ad.image.url)
-      # end
     end
 
   end
 
-
-    # panel "Company" do
-    #   if applicant.company
-    #     attributes_table_for applicant.company do
-    #       [:title, :legal_form, :phone, :fax, :homepage, :sector].each do |aa|
-    #         row aa
-    #       end
-    #     end
-    #     if applicant.company.location
-    #       attributes_table_for applicant.company.location do
-    #         [:street, :zip, :city, :region, :country].each do |aa|
-    #           row aa
-    #         end
-    #       end
-    #     end
-    #   end
-    # end
-    # panel "Registrations" do
-    #   table do
-    #     tr do
-    #       ["Event title", "Pricegroup title", "Price", "Registered at"].each do |sa|
-    #         th sa
-    #       end
-    #     end
-    #     applicant.event_registrations.each do |ereg|
-    #       tr do
-    #         [ereg.event_pricegroup.event.title, ereg.event_pricegroup.title, number_to_currency(ereg.event_pricegroup.price, :locale => :de), ereg.created_at].each do |esa|
-    #           td esa
-    #         end
-    #       end
-    #     end
-    #     tr :class => "total" do
-    #       td ""
-    #       td ""
-    #       td number_to_currency(applicant.total_price, :locale => :de)
-    #       td ""
-    #     end
-    #   end
-
-
-  # form :html => { :enctype => "multipart/form-data" } do |f|
-  #   f.inputs "Allgemein" do
-  #     f.input :title, :hint => "Der Titel des Panels, kann Leerzeichen und Sonderzeichen enthalten"
-  #     f.input :description, :hint => "Die Beschreibung des Panels"
-  #     f.input :link_url, :hint => "Link zur Panel-Seite"
-  #   end
-
-  #   f.inputs "Informationen" do
-  #     f.input :sponsors, :as => :check_boxes, :collection => GoldencobraEvents::Sponsor.find(:all, :order => "title ASC")
-  #   end
-
-  #   f.buttons
-  # end
 end
--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -8,21 +8,23 @@ module GoldencobraNewsletter
 
       @user = User.find_by_email(params[:email])
 
-      if(@user == nil)
-
+      if @user.present?
+        #ab ins model damit
         generated_pass = Digest::MD5.new.hexdigest("pass-#{Time.now.to_f}")
+        
         @user = User.create(:firstname => params[:first_name], :lastname => params[:last_name], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
 
       end
 
-      if(@user.newsletter_registration == nil)
+      if @user && @user.newsletter_registration == nil 
         @user.newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.new
+        @user.newsletter_registration.company_name = params[:company]
+        @user.newsletter_registration.is_subscriber = true
+
+        @success = @user.save
+
       end
 
-      @user.newsletter_registration.company_name = params[:company]
-      @user.newsletter_registration.is_subscriber = true
-
-      @success = @user.save
 
     end
 
