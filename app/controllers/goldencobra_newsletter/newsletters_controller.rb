d39d57c6ee1666c2825409264688845719e70882
Daniel Spaude <daniel@spaude.de>
2012-03-08 12:20:10 +0100

    newsletter stuff (controller)

--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -8,19 +8,24 @@ module GoldencobraNewsletter
 
       user = User.find_by_email(params[:email])
 
+      if user == nil
 
-      if user
-        user.newsletter = true
+        generated_pass = Digest::MD5.new.hexdigest("pass-#{Time.now.to_f}")
+        user = User.create(:firstname => params[:firstname], :lastname => [:lastname], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
 
         user.save!
 
-      else
-
-        generated_pass = Digest::MD5.new.hexdigest("pass-#{Time.now.to_f}")
-
-        user = User.create(:firstname => params[:firstname], :lastname => [:lastname], :email => params[:email], :password => generated_pass, :password_confirmation => generated_pass)
-
       end
+
+      if user.newsletter_registration == nil
+        user.newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.new
+      end
+
+      user.company_name = params[:company_name]
+      user.is_subscriber = true
+
+      user.save!
+
 
     end
 
--- app/models/goldencobra_newsletter/newsletter_registration.rb
+++ app/models/goldencobra_newsletter/newsletter_registration.rb
@@ -13,5 +13,7 @@
 module GoldencobraNewsletter
   class NewsletterRegistration < ActiveRecord::Base
     belongs_to :user, :class_name => User
+
+
   end
 end
