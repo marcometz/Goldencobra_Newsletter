48a47eb18290a56b419630639e2814c3fbbeba42
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-11 13:15:51 +0200

    modified:   config/initializers/active_admin.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,5 +1,5 @@
-ActiveAdmin.setup do |config|
-  config.load_paths << "#{GoldencobraNewsletter::Engine.root}/app/admin/"
-  config.load_paths = config.load_paths.uniq
-  config.register_stylesheet 'goldencobra_newsletter/active_admin'
-end
+#ActiveAdmin.setup do |config|
+#  config.register_stylesheet 'goldencobra_newsletter/active_admin'
+#end
+
+ActiveAdmin.application.load_paths << "#{GoldencobraNewsletter::Engine.root}/app/admin/"
