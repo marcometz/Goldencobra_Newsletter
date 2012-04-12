c3547f662fbbd54fc80f0818c854a3c118809468
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-12 14:31:40 +0200

    modified:   admin/articles.rb

--- admin/articles.rb
+++ admin/articles.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Goldencobra::Article, :as => "Article" do
-  menu parent: "Content-Management", label: "Artikel"
+  menu parent: "Content-Management", label: "Artikel", :if => proc{can?(:read, Goldencobra::Article)}
 
   sidebar :newsletter_module, only: [:edit] do
     render "/goldencobra_newsletter/admin/newsletters/newsletter_module_sidebar"
