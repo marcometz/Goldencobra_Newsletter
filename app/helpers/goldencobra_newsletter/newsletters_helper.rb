0f9f179bbf06343ff920f88a25741e2052b8e648
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-16 14:29:00 +0200

    modified:   app/helpers/goldencobra_newsletter/newsletters_helper.rb 	modified:   app/views/goldencobra_newsletter/newsletters/_register.html.erb

--- app/helpers/goldencobra_newsletter/newsletters_helper.rb
+++ app/helpers/goldencobra_newsletter/newsletters_helper.rb
@@ -1,9 +1,6 @@
 module GoldencobraNewsletter
   module NewslettersHelper
     def newsletter_registration_form
-      # render :text => "HIER KOMMT DAS TOLLE FORM REIN"
-
-      # return_content += content_tag(:div, render(:partial => "goldencobra_events/events/user"), :style => "display:none", :id => "goldencobra_events_enter_account_data_form")
       render :partial => "goldencobra_newsletter/newsletters/register" if @article && @article.newsletter
     end
 
