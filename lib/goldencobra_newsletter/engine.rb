1336b9ec234cacd44701da36de60c6051fb7a954
Marco Metz <marco.metz@gmail.com>
2012-05-16 10:36:16 +0200

    modified:   lib/goldencobra_newsletter/engine.rb

--- lib/goldencobra_newsletter/engine.rb
+++ lib/goldencobra_newsletter/engine.rb
@@ -6,11 +6,11 @@ module GoldencobraNewsletter
     config.to_prepare do
       ApplicationController.helper(GoldencobraNewsletter::ApplicationHelper)
       ActionController::Base.helper(GoldencobraNewsletter::ApplicationHelper)
-      DeviseController.helper(Goldencobra::ApplicationHelper)           
+      DeviseController.helper(GoldencobraNewsletter::ApplicationHelper)           
 
       ApplicationController.helper(GoldencobraNewsletter::NewslettersHelper)
       ActionController::Base.helper(GoldencobraNewsletter::NewslettersHelper)
-      DeviseController.helper(Goldencobra::NewslettersHelper)           
+      DeviseController.helper(GoldencobraNewsletter::NewslettersHelper)           
     end
 
   end
