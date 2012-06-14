485bcde27e233b53a2973d659350d7610ea38f71
Holger Frohloff <holger.frohloff@gmail.com>
2012-06-14 10:12:50 +0200

    modified:   app/controllers/goldencobra_newsletter/newsletters_controller.rb

--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -62,6 +62,18 @@ module GoldencobraNewsletter
         newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.where('user_id = ?', @user.id).first if @user
       end
       @article = Goldencobra::Article.find_by_title("newsletter-site")
+              Goldencobra::Article::LiquidParser["current_article"] = @article
+#TODO: in goldencobra application helper auslagern
+        set_meta_tags :site => s("goldencobra.page.default_title_tag"),
+                      :title => @article.metatag("Title Tag"),
+                      :description => @article.metatag("Meta Description"), 
+                      :keywords => @article.metatag("Keywords"),
+                      :canonical => @article.canonical_url,
+                      :noindex => @article.robots_no_index,
+                      :open_graph => {:title => @article.metatag("OpenGraph Title"), 
+                                    :type => @article.metatag("OpenGraph Type"),
+                                    :url => @article.metatag("OpenGraph URL"), 
+                                    :image => @article.metatag("OpenGraph Image")}
       if newsletter_registration && @user && newsletter_registration.newsletter_tags.include?(params[:tag])
         newsletter_registration.unsubscribe!(@user.email, params[:tag])
         render 'unsubscribe', layout: "application"
@@ -83,6 +95,18 @@ module GoldencobraNewsletter
       end
       if newsletter_registration && @user
         @article = Goldencobra::Article.find_by_title("newsletter-site")
+                Goldencobra::Article::LiquidParser["current_article"] = @article
+        
+        set_meta_tags :site => s("goldencobra.page.default_title_tag"),
+                      :title => @article.metatag("Title Tag"),
+                      :description => @article.metatag("Meta Description"), 
+                      :keywords => @article.metatag("Keywords"),
+                      :canonical => @article.canonical_url,
+                      :noindex => @article.robots_no_index,
+                      :open_graph => {:title => @article.metatag("OpenGraph Title"), 
+                                    :type => @article.metatag("OpenGraph Type"),
+                                    :url => @article.metatag("OpenGraph URL"), 
+                                    :image => @article.metatag("OpenGraph Image")}
         newsletter_registration.subscribe!(@user.email, params[:tag])
         render 'subscribe', layout: "application"
       else
