bf5cfc0cb150ff336b9460db3a58a55afc2100ee
Marco Metz <marco.metz@gmail.com>
2012-06-20 17:18:50 +0200

    modified:   app/controllers/goldencobra_newsletter/newsletters_controller.rb

--- app/controllers/goldencobra_newsletter/newsletters_controller.rb
+++ app/controllers/goldencobra_newsletter/newsletters_controller.rb
@@ -62,7 +62,19 @@ module GoldencobraNewsletter
         newsletter_registration = GoldencobraNewsletter::NewsletterRegistration.where('user_id = ?', @user.id).first if @user
       end
       @article = Goldencobra::Article.find_by_url_name("newsletter-site")
-      initialize_article(@article)
+      #initialize_article(@article)
+      Goldencobra::Article::LiquidParser["current_article"] = @article
+      set_meta_tags :site => s("goldencobra.page.default_title_tag"),
+                    :title => current_article.metatag("Title Tag"),
+                    :description => current_article.metatag("Meta Description"),
+                    :keywords => current_article.metatag("Keywords"),
+                    :canonical => current_article.canonical_url,
+                    :noindex => current_article.robots_no_index,
+                    :open_graph => {:title => current_article.metatag("OpenGraph Title"),
+                                  :type => current_article.metatag("OpenGraph Type"),
+                                  :url => current_article.metatag("OpenGraph URL"),
+                                  :image => current_article.metatag("OpenGraph Image")}
+      
       
       if newsletter_registration && @user && newsletter_registration.newsletter_tags.include?(params[:tag])
         newsletter_registration.unsubscribe!(@user.email, params[:tag])
@@ -85,7 +97,19 @@ module GoldencobraNewsletter
       end
       if newsletter_registration && @user
         @article = Goldencobra::Article.find_by_url_name("newsletter-site")
-        initialize_article(@article)
+        #initialize_article(@article)
+        Goldencobra::Article::LiquidParser["current_article"] = @article
+        set_meta_tags :site => s("goldencobra.page.default_title_tag"),
+                      :title => current_article.metatag("Title Tag"),
+                      :description => current_article.metatag("Meta Description"),
+                      :keywords => current_article.metatag("Keywords"),
+                      :canonical => current_article.canonical_url,
+                      :noindex => current_article.robots_no_index,
+                      :open_graph => {:title => current_article.metatag("OpenGraph Title"),
+                                    :type => current_article.metatag("OpenGraph Type"),
+                                    :url => current_article.metatag("OpenGraph URL"),
+                                    :image => current_article.metatag("OpenGraph Image")}
+        
 
         newsletter_registration.subscribe!(@user.email, params[:tag])
         render 'subscribe', layout: "application"
