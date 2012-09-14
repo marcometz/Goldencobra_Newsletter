ea8af13ff0de896cc712ddf1060164ee060163fa
Holger Frohloff <holger.frohloff@gmail.com>
2012-09-14 11:56:50 +0200

    stammdaten hinzugefuegt 	new file:   admin/master_datas.rb 	modified:   config/locales/activerecord.de.yml

---
+++ admin/master_datas.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register User, :as => "Master Data" do
+  controller.authorize_resource :class => User
+
+  index do
+    selectable_column
+    column :firstname, sortable: true
+    column :lastname, sortable: true
+    column :email, sortable: true
+    column :newsletter_registration do |user|
+      link_to "Newsletter Anmeldung bearbeiten", edit_admin_newsletter_registration_path(GoldencobraNewsletter::NewsletterRegistration.where(user_id: user.id).first) if GoldencobraNewsletter::NewsletterRegistration.where(user_id: user.id).any?
+    end
+    if ActiveRecord::Base.connection.table_exists?("goldencobra_events_registration_users")
+      column :event_registration do |user|
+        link_to "Event Anmeldungen", "/admin/applicants?q%5Bemail_contains%5D=#{user.email}&commit=Filtern&order=id_desc&scope=" if GoldencobraEvents::RegistrationUser.where(user_id: user.id).any?
+      end
+    end
+    default_actions
+  end
+
+end
