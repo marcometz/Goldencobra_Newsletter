97859f1e6ac54a738a9fc7e0296bfd5eeab966d9
Holger Frohloff <holger.frohloff@gmail.com>
2012-09-14 16:19:47 +0200

    Links und Form 	modified:   admin/master_datas.rb 	modified:   config/initializers/newsletter_registration.rb

--- admin/master_datas.rb
+++ admin/master_datas.rb
@@ -16,13 +16,58 @@ ActiveAdmin.register User, :as => "Master Data" do
     column :lastname, sortable: true
     column :email, sortable: true
     if ActiveRecord::Base.connection.table_exists?("goldencobra_events_registration_users")
-      column :event_registration, sortable: true do |user|
-        link_to "Event Anmeldungen", "/admin/applicants?q%5Bemail_contains%5D=#{user.email}&commit=Filtern&order=id_desc&scope=" if GoldencobraEvents::RegistrationUser.where(user_id: user.id).any?
+      column "Anmeldungen" do |user|
+        count = GoldencobraEvents::RegistrationUser.where(user_id: user.id).count
+        if count > 0
+          link_to "Liste von Event-Anmeldungen (#{count})", "/admin/applicants?q%5Bemail_contains%5D=#{user.email}&commit=Filtern&order=id_desc&scope="
+        end
       end
     end
-    column :newsletter_registration, sortable: true do |user|
-      link_to "Bearbeiten", edit_admin_newsletter_registration_path(GoldencobraNewsletter::NewsletterRegistration.where(user_id: user.id).first) if GoldencobraNewsletter::NewsletterRegistration.where(user_id: user.id).any?
+    column "" do |user|
+      if GoldencobraNewsletter::NewsletterRegistration.where(user_id: user.id).any?
+        link_to "Bearbeiten", edit_admin_newsletter_registration_path(GoldencobraNewsletter::NewsletterRegistration.where(user_id: user.id).first)
+      else
+        link_to "Bearbeiten", edit_admin_master_datum_path(user.id)
+      end
     end
   end
 
+ form :html => { :enctype => "multipart/form-data" }  do |f|
+    f.actions
+    f.inputs "Allgemein", class: "foldable inputs" do
+      f.input :gender, as: :select, include_blank: false, collection: [["Herr", true],["Frau", false]]
+      f.input :title
+      f.input :firstname
+      f.input :lastname
+      f.input :email
+      f.input :function
+      f.input :phone
+      f.input :fax
+      f.input :facebook
+      f.input :twitter
+      f.input :linkedin
+      f.input :xing
+      f.input :googleplus
+    end
+    # f.inputs "Newsletter" do
+    #   f.fields_for :user_attributes, f.object.user do |u|
+    #     u.inputs "" do
+    #       f.input :company_name
+    #       f.input :is_subscriber
+    #       f.input :newsletter_tags
+    #     end
+    #   end
+    # end
+    # f.inputs "Adresse" do
+    #   f.fields_for :location_attributes, f.object.location do |l|
+    #     l.inputs "" do
+    #       l.input :street
+    #       l.input :zip
+    #       l.input :city
+    #       l.input :country, as: :string
+    #     end
+    #   end
+    # end
+    f.actions
+  end
 end
--- config/initializers/newsletter_registration.rb
+++ config/initializers/newsletter_registration.rb
@@ -5,10 +5,10 @@ Rails.application.config.to_prepare do
     has_one :newsletter_registration, :class_name => GoldencobraNewsletter::NewsletterRegistration
 
     if ActiveRecord::Base.connection.table_exists?("goldencobra_events_registration_users")
-      scope :event_registration_present, joins(:registration_users).where("goldencobra_events_registration_users.user_id IS NOT NULL")
-      scope :event_registration_not_present, joins(:registration_users).where("goldencobra_events_registration_users.user_id IS NULL")
+      scope :event_registration_present, joins(:registration_users).where("goldencobra_events_registration_users.user_id IS NOT NULL").uniq
+      scope :event_registration_not_present, joins(:registration_users).where("goldencobra_events_registration_users.user_id IS NULL").uniq
 
-      scope :event_and_newsletter_registration_present, joins(:registration_users, :newsletter_registration).where("goldencobra_events_registration_users.user_id IS NOT NULL AND goldencobra_newsletter_newsletter_registrations.user_id IS NOT NULL")
+      scope :event_and_newsletter_registration_present, joins(:registration_users, :newsletter_registration).where("goldencobra_events_registration_users.user_id IS NOT NULL AND goldencobra_newsletter_newsletter_registrations.user_id IS NOT NULL").uniq
 
       scope :event_registration_present_eq, lambda{ |text| joins(:registration_users).where("goldencobra_events_registration_users.user_id = #{text}") }
       search_methods :event_registration_present_eq
